7df8195c6af8f601538282ea48881ea59f077e22
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-07 16:40:27 +0700

    edit slider photo on homepage

--- datas/index.ts
+++ datas/index.ts
@@ -20,24 +20,24 @@ export const gallerySlider = [
     image: '/projects/school-group-2.jpg',
   },
   {
-    name: 'Graduation',
-    image: '/projects/graduation.jpg',
+    name: 'Hunting-9',
+    image: '/projects/hunting/hunting-9.jpg',
   },
   {
-    name: 'Graduation 2',
-    image: '/projects/graduation-2.jpg',
+    name: 'Wedding-4',
+    image: '/projects/wedding/wedding-4.jpg',
   },
   {
-    name: 'School Photo Solo',
-    image: '/projects/school-solo.jpg',
+    name: 'Cosplay-1',
+    image: '/projects/cosplay/cosplay-1.jpg',
   },
   {
-    name: 'School Group',
-    image: '/projects/school-group.jpg',
+    name: 'Event',
+    image: '/projects/event-documentation/event-1.jpg',
   },
   {
-    name: 'School Group 2',
-    image: '/projects/school-group-2.jpg',
+    name: 'Hunting-4',
+    image: '/projects/hunting/hunting-4.jpg',
   },
 ];
 
@@ -68,6 +68,6 @@ export const CategoryPhoto = [
   },
   {
     name: 'Prewedding',
-    src: '/projects/prewedding-layout.png',
+    src: '/projects/wedding-layout.png',
   },
 ];
