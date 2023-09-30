a1bdfb17859c681e30ae13f29a9a1c2580d1b2cb
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-30 22:12:37 +0700

    update version of next js

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,27 +4,27 @@ import Carousel from '../components/carousel';
 
 export default async function Projects() {
   return (
-    <div className="relative w-full overflow-scroll md:overflow-hidden">
+    <div className="md:overflow-hidden relative w-full overflow-scroll">
       <Navigation />
 
-      <div className="flex flex-col px-6 pt-20 mx-auto space-y-6 overflow-scroll md:overflow-hidden max-w-7xl lg:px-8 md:space-y-10 md:pt-24 lg:pt-32 animate-fade-in-up">
-        <div className="mx-auto md:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+      <div className="md:overflow-hidden max-w-7xl lg:px-8 md:space-y-10 md:pt-24 lg:pt-32 animate-fade-in-up flex flex-col px-6 pt-20 mx-auto space-y-6 overflow-scroll">
+        <div className="md:mx-0 mx-auto">
+          <h2 className="text-zinc-100 sm:text-4xl text-3xl font-bold tracking-tight">
             Projects
           </h2>
 
-          <p className="mt-4 text-zinc-400">
+          <p className="text-zinc-400 mt-4">
             Some of my projects that I have worked on in the past.
           </p>
         </div>
 
-        <div className="w-full h-px bg-zinc-800" />
+        <div className="bg-zinc-800 w-full h-px" />
 
-        <div className="flex flex-col items-center w-full gap-5 mx-auto overflow-hidden md:overflow-auto md:flex-wrap md:flex-row">
+        <div className="md:overflow-auto md:flex-wrap md:flex-row flex flex-col items-center w-full gap-5 mx-auto overflow-hidden">
           <Carousel />
         </div>
 
-        <div className="w-full h-px bg-zinc-800" />
+        <div className="bg-zinc-800 w-full h-px" />
       </div>
     </div>
   );
